8b7e8a874aa705ee62e6a457e2632c6df29d5bef
Shylendra Madda <shylendra.madda@tylertech.com>
2020-07-08 13:08:47 +0530

    Intial commit

---
+++ src/main/java/com/techatcore/ShareFile/ShareFileApplication.java
@@ -0,0 +1,13 @@
+package com.techatcore.ShareFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareFileApplication.class, args);
+	}
+
+}
